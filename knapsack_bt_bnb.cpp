7f405fac3919061f0ec85f6182d29a9b045714da
Daniel Ricci <ra145781@titas.lab.ic.unicamp.br>
2017-04-20 18:05:15 -0300

    Codigo nao compilavel

--- knapsack_bt_bnb.cpp
+++ knapsack_bt_bnb.cpp
@@ -6,15 +6,86 @@
 #include "knapsack.h"
 
 ///Preencher aqui para facilitar a correcao. 
-// Nome1:
-// RA1:
-// Nome2: 
-// RA2:
+// Nome1: Daniel Ricci
+// RA1: 145781
+// Nome2: Bleno Humberto Claus
+// RA2: 145444
+
+int checkSolution(vector<int> sol, vector<int> p, vector<int> w, vector<int> c, int B, int d){
+	
+	int banda = 0, nclasses = 0, pgto = 0;
+	vector<int> classes(10000, 0);
+	
+	for(int i = 0; i < sol.size(); i++){
+		if(sol[i] == 1){
+			if(classes[c[i]] == 0){
+				classes[c[i]] = 1;
+				nclasses++;
+			}
+			banda += w[i];
+			pgto += p[i];	
+		}
+	}
+	
+	banda += (nclasses - 1) * d;
+	
+	if(banda <= B) return pgto;
+	else return -1;
+}
+
+void printVector(vector<int> v){
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
 ///
 // Bactracking function: 
 ///
 bool bt(int n, int d, int B, vector<int> &p, vector<int> &w, vector<int> &c, vector<int> &sol, int t){
+	
+	int curBstValue = 0;
+	vector<int> curBstSol(sol.size(), 0);
+	fill(sol.begin(), sol.end(), 0);
+	
+	for(int i = 0; i < n; i++){
+		sol[i] = 1;
+		int aux = checkSolution(sol, w, c, B, d);
+		if(aux > -1){
+		  	if(aux > curBstSol){
+				curBstValue = aux;
+				curBstSol = sol;
+			}
+		} else {
+			sol[i] = 0;
+			continue;			
+		}
+		bt(n,d,B,p,w,c,sol,t);
+		sol[i] = 0;
+	}
+	
 	return false;
+}
+
+int backtrack(int n, int d, int B, vector<int> &p, vector<int> &w, vector<int> &c, vector<int> &sol, int t, int k){
+	
+	if(k == n){
+		return checkSolution(sol, p, w, c, B, d);	
+	}
+	
+	vector<int> solAux = sol ;	
+	int aux = checkSolution(sol, w, c, B, d);
+	if(aux > -1){
+	  	if(aux > curBstSol){
+			curBstValue = aux;
+			curBstSol = sol;
+		}
+	}
+		
+	solAux[k] = 1;
+	return max(backtrack(n, d, B, p, w, c, sol, t, k+1),backtrack(n, d, B, p, w, c, solAux, t, k+1);  );
+
 }
 
 ///
